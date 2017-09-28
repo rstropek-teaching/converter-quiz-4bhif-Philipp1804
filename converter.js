614356fc387ebde02de464d72ee17289cd2c63f0
Rainer Stropek <rainer@timecockpit.com>
2017-09-28 14:14:22 +0200

    Add tips

--- converter.js
+++ converter.js
@@ -1,8 +1,13 @@
+// In order to get the tests green, rename `converter.js` to `convert.js`
+
+// Consider parsing the parameter with `parseFloat` in the next line. You can check the result
+// for falsynes to find out if the parameter is a float.
 var num = process.argv[2];
 var akt = process.argv[3];
 var to = process.argv[5];
 var help = num;
 
+// Why didn't you use another `switch` instead of the many if's here?
 if(akt == 'm'){
     switch(to){
         case 'mm': num=num*10;
@@ -30,6 +35,7 @@ if(akt == 'g'){
     if(to == 'kg')num=num/1000;
 }
 if(num == help){
+    // Requirements contained a different error message.
     console.error("Statement is false");
 }else{
    console.log(num+" "+to); 
